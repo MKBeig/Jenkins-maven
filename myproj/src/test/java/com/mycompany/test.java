91b29e133a4be3a3085198b187d33ac671d4f6bd
user1 <user@gmail.com>
2024-02-01 12:39:44 +0530

    jenkinsproj

---
+++ myproj/src/test/java/com/mycompany/test.java
@@ -0,0 +1,13 @@
+package com.mycompany;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MyAppTest {
+
+    @Test
+    public void testApp() {
+        MyApp app = new MyApp();
+        assertNotNull(app);
+    }
+}
